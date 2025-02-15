5553bc89c65cf928dec79796159f87a983875cad
HANAI tohru <tohru@reedom.com>
2025-02-15 16:48:16 +0900

    build: lib

--- lib/api/KeepaAPI.d.ts
+++ lib/api/KeepaAPI.d.ts
@@ -16,9 +16,27 @@ export declare class KeepaAPI {
     private httpClient;
     private maxDelay;
     private defaultTimeout;
+    /**
+     * Creates an instance of KeepaAPI.
+     *
+     * @param {Object} options - Configuration options for the client.
+     * @param {string} options.accessKey - Your KeepaAPI access key.
+     * @param {'auto' | 'axios' | 'gas' | KeepaHttpClient} [options.httpClient='auto']
+     *   - The HTTP client to use. Options:
+     *     - `'auto'`: Automatically selects a suitable HTTP client based on the environment.
+     *     - `'axios'`: Uses the Axios library for HTTP requests.
+     *     - `'gas'`: Uses Google Apps Script's `UrlFetchApp` for HTTP requests.
+     *     - `KeepaHttpClient`: A custom HTTP client implementation.
+     * @param {string} [options.userAgent='KEEPA-JAVA Framework-0.1.0']
+     *   - The user agent string to send with requests.
+     * @param {number} [options.maxDelay=60000]
+     *   - The maximum delay (in milliseconds) between requests.
+     * @param {number} [options.defaultTimeout=120000]
+     *   - The default timeout (in milliseconds) for requests.
+     */
     constructor({ accessKey, httpClient, userAgent, maxDelay, defaultTimeout, }: {
         accessKey: string;
-        httpClient?: 'auto' | 'axios' | 'UrlFetchApp' | KeepaHttpClient;
+        httpClient?: 'auto' | 'axios' | 'gas' | KeepaHttpClient;
         userAgent?: string;
         maxDelay?: number;
         defaultTimeout?: number;
---
+++ lib/api/http/KeepaHttpClientGAS.d.ts
@@ -0,0 +1,4 @@
+import { KeepaHttpClient } from './KeepaHttpClient';
+export declare function createKeepaHttpClientGAS({ userAgent, }: {
+    userAgent: string;
+}): KeepaHttpClient;
