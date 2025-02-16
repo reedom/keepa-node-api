35ad4baf847f30c3c8ed2b70a7fba895cbf0c4d8
HANAI tohru <tohru@reedom.com>
2025-02-16 16:40:54 +0900

    refactor: model field types

--- src/api/models/Stats.ts
+++ src/api/models/Stats.ts
@@ -9,49 +9,49 @@ export type Stats = {
    * The price is an integer of the respective Amazon locale's smallest currency unit (e.g., euro cents or yen).
    * If no offer was available in the given interval (e.g., out of stock), the price has the value -1.
    */
-  current?: number[];
+  current: number[];
 
   /**
    * Contains the weighted mean for the interval specified in the product request's stats parameter.
    * Uses {@link CsvType} indexing.
    * If no offer was available in the given interval or there is insufficient data, it has the value -1.
    */
-  avg?: number[];
+  avg: number[];
 
   /**
    * Contains the weighted mean for the last 30 days.
    * Uses {@link CsvType} indexing.
    * If no offer was available in the given interval or there is insufficient data, it has the value -1.
    */
-  avg30?: number[];
+  avg30: number[];
 
   /**
    * Contains the weighted mean for the last 90 days.
    * Uses {@link CsvType} indexing.
    * If no offer was available in the given interval or there is insufficient data, it has the value -1.
    */
-  avg90?: number[];
+  avg90: number[];
 
   /**
    * Contains the weighted mean for the last 180 days.
    * Uses {@link CsvType} indexing.
    * If no offer was available in the given interval or there is insufficient data, it has the value -1.
    */
-  avg180?: number[];
+  avg180: number[];
 
   /**
    * Contains the weighted mean for the last 365 days.
    * Uses {@link CsvType} indexing.
    * If no offer was available in the given interval or there is insufficient data, it has the value -1.
    */
-  avg365?: number[];
+  avg365: number[];
 
   /**
    * Contains the prices registered at the start of the interval specified in the product request's stats parameter.
    * Uses {@link CsvType} indexing.
    * If no offer was available in the given interval or there is insufficient data, it has the value -1.
    */
-  atIntervalStart?: number[];
+  atIntervalStart: number[];
 
   /**
    * Contains the lowest prices registered for this product.
@@ -60,7 +60,7 @@ export type Stats = {
    * an array of size 2, where the first value is the time of the extreme point (in Keepa time minutes) and the second is the respective extreme value.
    * Use {@link KeepaTime.keepaMinuteToUnixInMillis} to get an uncompressed timestamp (Unix epoch time).
    */
-  min?: number[][];
+  min: number[][];
 
   /**
    * Contains the lowest prices registered in the interval specified in the product request's stats parameter.
@@ -69,7 +69,7 @@ export type Stats = {
    * an array of size 2, where the first value is the time of the extreme point (in Keepa time minutes) and the second is the respective extreme value.
    * Use {@link KeepaTime.keepaMinuteToUnixInMillis} to get an uncompressed timestamp (Unix epoch time).
    */
-  minInInterval?: number[][];
+  minInInterval: number[][];
 
   /**
    * Contains the highest prices registered for this product.
@@ -78,7 +78,7 @@ export type Stats = {
    * an array of size 2, where the first value is the time of the extreme point (in Keepa time minutes) and the second is the respective extreme value.
    * Use {@link KeepaTime.keepaMinuteToUnixInMillis} to get an uncompressed timestamp (Unix epoch time).
    */
-  max?: number[][];
+  max: number[][];
 
   /**
    * Contains the highest prices registered in the interval specified in the product request's stats parameter.
@@ -87,35 +87,35 @@ export type Stats = {
    * an array of size 2, where the first value is the time of the extreme point (in Keepa time minutes) and the second is the respective extreme value.
    * Use {@link KeepaTime.keepaMinuteToUnixInMillis} to get an uncompressed timestamp (Unix epoch time).
    */
-  maxInInterval?: number[][];
+  maxInInterval: number[][];
 
   /**
    * Whether the current price is the all-time lowest price.
    * Uses {@link CsvType} indexing.
    */
-  isLowest?: boolean[];
+  isLowest: boolean[];
 
   /**
    * Whether the current price is the lowest price in the last 90 days.
    * Uses {@link CsvType} indexing.
    */
-  isLowest90?: boolean[];
+  isLowest90: boolean[];
 
   /**
    * Number of times in the last 30 days Amazon went out of stock.
    */
-  outOfStockCountAmazon30?: number;
+  outOfStockCountAmazon30: number;
 
   /**
    * Number of times in the last 90 days Amazon went out of stock.
    */
-  outOfStockCountAmazon90?: number;
+  outOfStockCountAmazon90: number;
 
   /**
    * Contains the difference in percent between the current monthlySold value and the average value of the last 90 days.
    * The value `0` means it did not change or could not be calculated.
    */
-  deltaPercent90_monthlySold?: number;
+  deltaPercent90_monthlySold: number;
 
   /**
    * Contains the out-of-stock percentage in the interval specified in the product request's stats parameter.
@@ -123,7 +123,7 @@ export type Stats = {
    * It has the value `-1` if there is insufficient data or the CsvType is not a price.
    * Examples: `0 = never was out of stock`, `100 = was out of stock 100% of the time`, `25 = was out of stock 25% of the time`
    */
-  outOfStockPercentageInInterval?: number[];
+  outOfStockPercentageInInterval: number[];
 
   /**
    * Contains the 90-day out-of-stock percentage.
@@ -131,7 +131,7 @@ export type Stats = {
    * It has the value `-1` if there is insufficient data or the CsvType is not a price.
    * Examples: `0 = never was out of stock`, `100 = was out of stock 100% of the time`, `25 = was out of stock 25% of the time`
    */
-  outOfStockPercentage90?: number[];
+  outOfStockPercentage90: number[];
 
   /**
    * Contains the 30-day out-of-stock percentage.
@@ -139,7 +139,7 @@ export type Stats = {
    * It has the value `-1` if there is insufficient data or the CsvType is not a price.
    * Examples: `0 = never was out of stock`, `100 = was out of stock 100% of the time`, `25 = was out of stock 25% of the time`
    */
-  outOfStockPercentage30?: number[];
+  outOfStockPercentage30: number[];
 };
 
 /**
